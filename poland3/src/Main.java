33337b3fd140328b928d6a64a010e939241eafb7
Seval Ersoy <seval25ersoy@outlook.com>
2025-12-04 00:48:34 +0100

    Feature: Table creation and display added

---
+++ poland3/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        System.out.println("Table elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element " + i + ": " + numbers[i]);
+        }
+    }
+}
+
