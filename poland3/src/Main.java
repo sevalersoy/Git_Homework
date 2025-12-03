6fb55bb3a17aaa0a4fd33ac86d484672c9f038de
Seval Ersoy <seval25ersoy@outlook.com>
2025-12-04 00:54:16 +0100

    Feature: Table initialized with random values

--- poland3/src/Main.java
+++ poland3/src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[10];
 
+        Random random = new Random();
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = random.nextInt(100);
+        }
+
         System.out.println("Table elements:");
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             System.out.println("Element " + i + ": " + numbers[i]);
         }
     }
