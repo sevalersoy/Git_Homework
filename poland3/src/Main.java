d8adc3613efbed3d20be4ca991349ba4412cf05f
Seval Ersoy <seval25ersoy@outlook.com>
2025-12-04 01:08:08 +0100

    Feature: Array sorting algorithm added

--- poland3/src/Main.java
+++ poland3/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[10];
@@ -7,6 +9,7 @@ public class Main {
         for(int i = 0; i < numbers.length; i++){
             numbers[i] = random.nextInt(100);
         }
+        Arrays.sort(numbers);
 
         System.out.println("Table elements:");
 
